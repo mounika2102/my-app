f0ad05930206d57f5ac2fbabe5fe80e7a7541b56
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-06 20:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def home():
     app_name = os.getenv("APP_NAME", "Default App")
     return f"Hello from {app_name} 🚀"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
