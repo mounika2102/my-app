a65f0eb0c606a2dacfd95b6e41db107d15d4113e
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-06 20:23:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Moki from OpenShift 🚀"
+    app_name = os.getenv("APP_NAME", "Default App")
+    return f"Hello from {app_name} 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
