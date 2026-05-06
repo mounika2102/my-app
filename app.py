cf6695fc868f5b579d5b116e539c794ae9da4853
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-06 14:59:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-app = Flask(_name_)
 
-@app.route('/')
-def hello():
-    return "Hello from Docker CI/CD!"
+app = Flask(__name__)
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return "Hello from OpenShift 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
