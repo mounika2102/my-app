df07417c58a2ce28881ebc4dd623bee6ee38c0df
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-11 18:42:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from flask import Flask
 from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
 
 REQUEST_COUNT = Counter(
     'app_requests_total',
@@ -12,7 +15,16 @@ def home():
 
     return "Hello OpenShift"
 
+@app.route("/health")
+def health():
+
+    return "OK", 200
+
 @app.route("/metrics")
 def metrics():
 
     return generate_latest()
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000)
