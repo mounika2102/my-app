fd6406d55b2920b6e315540c1777751dee9038b8
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-11 20:12:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -184,3 +184,30 @@ EOF
 EOT
   }
 }
+resource "kubernetes_manifest" "servicemonitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+
+    metadata = {
+      name      = "myfirstd-monitor"
+      namespace = "dev"
+    }
+
+    spec = {
+      selector = {
+        matchLabels = {
+          app = "myfirstd"
+        }
+      }
+
+      endpoints = [
+        {
+          port     = "http"
+          path     = "/metrics"
+          interval = "30s"
+        }
+      ]
+    }
+  }
+}
