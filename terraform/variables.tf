2e0181bae67cf1bea2de17724c82e667e15ffc23
mounika2102 <99018771+mounika2102@users.noreply.github.com>
2026-05-11 18:54:37 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,32 +28,3 @@ variable "environment" {
 
   default = "dev"
 }
-variable "openshift_server" {
-
-  description = "OpenShift API URL"
-}
-
-variable "openshift_token" {
-
-  description = "OpenShift login token"
-  sensitive   = true
-}
-
-variable "namespace" {
-
-  default = "mounika-red-dev"
-}
-
-variable "app_name" {
-
-  default = "funcapp"
-}
-
-variable "image" {
-  description = "Docker image"
-}
-
-variable "environment" {
-
-  default = "dev"
-}
